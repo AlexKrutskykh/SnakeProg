3efc9479b0cdc0960e57096db7835fb1a5f8bf78
Alexk <alexkrutskih@gmail.com>
2021-10-26 01:27:13 -0400

    Add project files.

---
+++ SnakeProg/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
