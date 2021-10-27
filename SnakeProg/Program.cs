5f2cc708ec6e9593e5a540b40e071ab7d9fb824e
Alexk <alexkrutskih@gmail.com>
2021-10-26 21:31:33 -0400

    solution additional task / with 100 iterations
    Home taks

--- SnakeProg/Program.cs
+++ SnakeProg/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SnakeProg
 {
@@ -6,10 +7,40 @@ namespace SnakeProg
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"What is your name?");
+            /*Console.WriteLine($"What is your name?");
             var userName = Console.ReadLine();
-            Console.WriteLine($"Hello {userName}, how are you dude? ");
-            
+            Console.WriteLine($"Hello {userName}, how are you dude? ");*/
+            task1();
         }
+        static void task1() {
+
+            var fizz = "Fizz"; //devided on 3 %
+            var buzz = "Buzz"; //devided on 5 %
+            var fizzAndbuzz = "Fizz + Buzz";
+          
+
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(fizzAndbuzz);
+                }
+                else if (i % 3 == 0 )
+                {
+                    Console.WriteLine(fizz);
+                }
+                else if  (i % 5 == 0)
+                {
+                    Console.WriteLine(buzz);
+                }
+                else {
+                    Console.WriteLine(i);
+                }
+
+                
+            }
+
+        } 
+            
     }
 }
