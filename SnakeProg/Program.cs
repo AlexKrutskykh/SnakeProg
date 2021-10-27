af83ccb50bfc13c8c875ad90ca48cfe525da75c6
Alexk <alexkrutskih@gmail.com>
2021-10-27 18:57:52 -0400

    Start of snake code

--- SnakeProg/Program.cs
+++ SnakeProg/Program.cs
@@ -6,9 +6,31 @@ namespace SnakeProg
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
-            Console.WriteLine("Test message after");
+            var x1 = 3;
+            var y1 = 4;
+            var sym1 = '*';
+
+            Drow(x1, y1, sym1);
+
+            var x2 = 4;
+            var y2 = 7;
+            var sym2 = '#';
+
+
+            Drow( x2, y2, sym2);
+
+
         }
+
+        static void Drow (int x , int y , char sym )
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+
+        }
+
+
+
+
     }
 }
