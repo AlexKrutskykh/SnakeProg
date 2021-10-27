8689b07977d98b85e2bcc6e684392a14e17d8840
Alexk <alexkrutskih@gmail.com>
2021-10-27 00:19:36 -0400

    task 2 done

--- SnakeProg/Program.cs
+++ SnakeProg/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace SnakeProg
 {
@@ -11,36 +10,46 @@ namespace SnakeProg
             var userName = Console.ReadLine();
             Console.WriteLine($"Hello {userName}, how are you dude? ");*/
             task1();
+            task2();
         }
-        static void task1() {
+        static void task1()
+        {
 
             var fizz = "Fizz"; //devided on 3 %
             var buzz = "Buzz"; //devided on 5 %
             var fizzAndbuzz = "Fizz + Buzz";
-          
 
-            for (int i = 1; i < 100; i++)
+
+            for (int i = 1; i <= 100; i++)
             {
                 if (i % 3 == 0 && i % 5 == 0)
                 {
                     Console.WriteLine(fizzAndbuzz);
                 }
-                else if (i % 3 == 0 )
+                else if (i % 3 == 0)
                 {
                     Console.WriteLine(fizz);
                 }
-                else if  (i % 5 == 0)
+                else if (i % 5 == 0)
                 {
                     Console.WriteLine(buzz);
                 }
-                else {
+                else
+                {
                     Console.WriteLine(i);
                 }
 
-                
-            }
 
-        } 
-            
+            }
+        }
+
+        static void task2()
+        {
+            Console.WriteLine($"Enter your name!");
+            var username = Console.ReadLine();
+            Console.WriteLine($"Hi, {username}, today is {DateTime.Now}");
+
+        }
+
     }
 }
